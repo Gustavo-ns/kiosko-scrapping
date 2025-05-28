fc1957f58b482c7341f4345d7d902db509c417d6
GBinfragistics <GBentancor@infragistics.com>
2025-05-28 14:57:34 -0300

    Elimine el BK

--- BK/app/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$router->get('/', 'HomeController@index');
-$router->get('/scrape', 'ScraperController@scrape');
-$router->get('/update', 'MeltwaterController@update'); 
--- BK/create_task.ps1
+++
@@ -1,4 +0,0 @@
-$Action = New-ScheduledTaskAction -Execute "C:\MAMP\htdocs\kiosko-scrapping\update_task.bat"
-$Trigger = New-ScheduledTaskTrigger -Daily -At "7:00AM"
-$Settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -DontStopOnIdleEnd -AllowStartIfOnBatteries
-$Task = Register-ScheduledTask -TaskName "KioskoScraping" -Action $Action -Trigger $Trigger -Settings $Settings -Description "Actualiza el scraping de portadas todos los días a las 7:00 AM hora de Uruguay" 
