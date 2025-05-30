9292d9e4112b279988e8fce65f0a337e2bebf5b7
GBinfragistics <GBentancor@infragistics.com>
2025-05-30 15:09:26 -0300

    Se limpiaron los TEST + LOGS

--- update_cron.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d %~dp0
-"C:\MAMP\bin\php\php8.2.0\php.exe" update_all.php >> cron_update.log 2>&1 
--- update_cron.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-/usr/bin/php update_all.php >> cron_update.log 2>&1 
